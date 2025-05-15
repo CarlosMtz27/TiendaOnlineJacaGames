a84b8a53f3178a916bcb174e99ae5c4e5c8bc51b
CarlosMtz27 <carlosmartinezhernandez1930@gmail.com>
2025-05-14 19:17:14 -0600

    Se configuol ruta para las imagenes

--- src/main/java/com/uacm/pixelpalace/ResourceWebConfiguration.java
+++ src/main/java/com/uacm/pixelpalace/ResourceWebConfiguration.java
@@ -7,8 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResourceWebConfiguration implements WebMvcConfigurer{
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	/*public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+	}*/
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Imágenes estáticas (CSS, JS, Imágenes predeterminadas)
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
 	}
-
 }
